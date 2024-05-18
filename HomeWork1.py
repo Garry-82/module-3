a158e76a36eeac40a1ec42b6e8df74d1b4f05662
Garry-82 <168720206+Garry-82@users.noreply.github.com>
2024-05-18 11:36:12 +0300

    Add files via upload

--- HomeWork1.py
+++ HomeWork1.py
@@ -16,3 +16,7 @@ value_dict = {"a": True,"b": "bbb","c": [3, 3, 3]}
 print("Передача распакованных элементов Списка в параметры функции:  ")
 print_param(*value_list)
 print_param(**value_dict)
+
+value_list2 = [222,"---Список2---"]
+print("проверка работы функции со списком из двух элементов и присвоенным параметром")
+print_param(*value_list2, "ccc")
